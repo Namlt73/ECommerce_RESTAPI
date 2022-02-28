858eac53c213b10c28786b45d36c398861851ac3
J_Nam <NamltGCD18728@fpt.edu.vn>
2022-02-28 19:44:03 +0700

    Add ICommentService interface

---
+++ ApiEcommerce/Dtos/CommentDtos/CommentDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiEcommerce.Dtos.CommentDtos
+{
+    public class CommentDto
+    {
+        public string Content { get; set; }
+    }
+}
---
+++ ApiEcommerce/Services/Interfaces/ICommentService.cs
@@ -0,0 +1,15 @@
+﻿using ApiEcommerce.Dtos.CommentDtos;
+using ApiEcommerce.Entities;
+using System.Threading.Tasks;
+
+namespace ApiEcommerce.Services.Interfaces
+{
+    interface ICommentService
+    {
+        Task<Comment> GetCommentById(long id, bool includeUser = false);
+        Task<Comment> AddComment(User user, string productSlug, CommentDto commentDto,
+            long userId);
+        Task<int> DeleteComment(long id);
+        Task<int> UpdateComment(Comment comment, CommentDto commentDto);
+    }
+}
